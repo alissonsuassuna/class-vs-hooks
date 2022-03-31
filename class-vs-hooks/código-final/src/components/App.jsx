bca2e528cede73cd3c997420d9c35c2a80dc4aa2
alissonsuassuna <alissonsuassuna@gmail.com>
2022-03-31 10:33:21 -0300

    first commit

---
+++ class-vs-hooks/código-final/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import ClassComponent from "./ClassComponent";
+
+//function App() {
+ //   return <h1>Jesus te ama</h1>
+//}
+
+class App extends React.Component {
+
+    render() {
+        return <ClassComponent />
+    }
+}
+
+export default App;
